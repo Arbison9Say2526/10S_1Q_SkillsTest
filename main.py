06c8c224160b41972573b80a0a1c4191c76a1fde
Arbison9Say2526 <arbison.ab@obmontessori.edu.ph>
2026-09-14 12:23:26 +0800

    Refactor order function to calculate total and VAT

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from pyscript import document, display
+from pyscript import document
 
 def order(e): 
-    document.getElementById('result').innerHTML = "order"
-    DHP = float(document.getElementById('DHP').value) 
-    POT = float(document.getElementById('POT').value)
-    GMT = float(document.getElementById('GMT').value)
-    OOM = float(document.getElementById('OOM').value)
-    DCT = float(document.getElementById('DCT').value)
-    JBC = float(document.getElementById('JBC').value)  
-    subtotal = float(DHP.value) * DHP.checked + float(POT.value) * POT.checked + float(GMT.value) * GMT.checked + float(OOM.value) * OOM.checked + float(DCT.value) * DCT.checked + float(JBC.value) * JBC.checked
+    item1 = float(document.getElementById('item1').value)
+    item2 = float(document.getElementById('item2').value) 
+    item3 = float(document.getElementById('item3').value) 
+    item4 = float(document.getElementById('item4').value) 
+    item5 = float(document.getElementById('item5').value)
+    item6 = float(document.getElementById('item6').value) 
+    subtotal = (float(item1) * document.getElementById('item1').checked + 
+                float(item2) * document.getElementById('item2').checked + 
+                float(item3) * document.getElementById('item3').checked + 
+                float(item4) * document.getElementById('item4').checked + 
+                float(item5) * document.getElementById('item5').checked + 
+                float(item6) * document.getElementById('item6').checked)
 
     vat = subtotal * 0.12
-    grandtotal = subtotal + vat
+    total = subtotal + vat
+    
 
-    display(f'VAT: {vat}', target='show')
-    display(f'Your Total is {grandtotal}', target='show')
+
+    document.getElementById('subtotal').innerHTML = f"{subtotal}"
+    document.getElementById('vat').innerHTML = f"{vat}"
+    document.getElementById('total').innerHTML = f"{total}"
